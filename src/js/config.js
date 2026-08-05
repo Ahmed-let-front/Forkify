9dc4fa049a202280a1ba93ba44beea7eb3634dc3
Ahmed <letcosdgp@gmail.com>
2026-08-06 01:56:18 +0300

    fix some bug

--- src/js/config.js
+++ src/js/config.js
@@ -2,4 +2,4 @@ export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes';
 export const DECIMAL_BASE = 10;
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 7;
-export const KEY = '977b5c52-0991-4c08-86ed-b2cb2ba52d38';
+export const KEY = '3a0183eb-b5f9-4b8d-a19e-d6a0d701a7b0';
